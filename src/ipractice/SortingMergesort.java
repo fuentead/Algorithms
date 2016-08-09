c2be63f1e55054c4a36a95773eb1f96d4b9d4c4c
Adriana <afuentes321@gmail.com>
2016-08-08 21:17:31 -0700

    Mergesort algorithm

---
+++ src/ipractice/SortingMergesort.java
@@ -0,0 +1,82 @@
+package ipractice;
+
+/**
+ * @author Adriana Fuentes
+ *
+ */
+public class SortingMergesort {
+	
+	int[] data;
+	int[] aux;
+	int size;
+	
+	public SortingMergesort(int[] data) {
+		this.data = data;
+		this.size = data.length;
+		this.aux = new int[size];
+	}
+	
+	public int[] mergesort() {
+		if(size < 1)
+			return data;
+		
+		sort(0, size-1);		
+		return data;		
+	}
+	
+	private void sort(int low, int high) {
+		if(low < high) {
+			int mid = low + ((high-low))/2;
+			
+			System.out.println("mid: " + mid + " high: " + high + " low: " + low);
+			sort(low, mid);
+			sort(mid+1, high);
+			merge(low, mid, high);
+		}
+	}
+	
+	private void merge(int low, int mid, int high) {
+		
+		// Copy data to auxiliary array
+		for(int i=low; i<=high; i++)
+			aux[i] = data[i];
+		
+		int i = low; // low index for aux
+		int j = mid + 1; // high index for aux
+		int k = low; // index for data array
+		
+		while(i <= mid && j <= high) {
+			// Compare low & mid from aux to decide 
+			// the smallest value that can be 
+			// put first in data array
+			if(aux[i] <= aux[j]) {
+				data[k] = aux[i];
+				i++;
+			}
+			else {
+				data[k] = aux[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while(i <= mid) {
+			data[k] = aux[i];
+			i++;
+			k++;
+		}		
+	}
+
+	public void showData() {
+		for(int i=0; i< data.length; i++)
+			System.out.print(" " + data[i]);
+	}
+	
+	public static void main(String[] args) {
+		int [] data = {3, 5, 1, 8, 2, 9, 0};
+		
+		SortingMergesort ms = new SortingMergesort(data);
+		ms.mergesort();
+		ms.showData();		
+	}
+}
