a89f714d8275944dde731ec6b7c3b71786c241ca
Adriana <afuentes321@gmail.com>
2016-11-28 16:53:51 -0800

    Rainfall Challenge initial commit

---
+++ src/ipractice/GraphsRainfallChallenge.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ipractice;
+
+/**
+ * @author adrianafuentes
+ *
+ */
+public class GraphsRainfallChallenge {
+
+	/**
+	 * 
+	 */
+	public GraphsRainfallChallenge() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
