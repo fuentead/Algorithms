4d8f630a42e1393ec403437762444a31be0d8bd4
Adriana <afuentes321@gmail.com>
2016-11-28 16:50:07 -0800

    Skip List initial commit

---
+++ src/ipractice/GraphsSkipList.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ipractice;
+
+/**
+ * @author adrianafuentes
+ *
+ */
+public class GraphsSkipList {
+
+	/**
+	 * 
+	 */
+	public GraphsSkipList() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
