7f06930e245ecf3005581876c81a70255fb93978
Adriana <afuentes321@gmail.com>
2017-02-02 15:46:12 -0800

    CountHi problem solved using recursion.

---
+++ src/ipractice/RecCountHi.java
@@ -0,0 +1,42 @@
+package ipractice;
+
+/**
+ * @author adrianafuentes
+ * 
+ * Given a string, compute recursively (no loops) the number 
+ * of times lowercase "hi" appears in the string.
+ * 
+ * countHi("xxhixx") → 1
+ * countHi("xhixhix") → 2
+ * countHi("hi") → 1
+ */
+public class RecCountHi {
+
+
+   /**
+    * CountHi problem resolved by recursion
+    * 
+    * Time Complexity: O(n)
+    * Space Complexity: O(n)
+    * @param string
+    */
+   public static int recCountHi(String s) {
+      if(s.length() < 2)
+         return 0;
+      
+      String letters = s.substring(s.length()-2);     
+      if(letters.equals("hi")) {
+         String word = s.substring(0, s.length()-2);
+         return 1 + recCountHi(word);
+      }
+      else {
+         String word = s.substring(0, s.length()-1);
+         return recCountHi(word);  
+      }
+   }
+   
+   public static void main(String[] args) {
+      int res = recCountHi("hihihellohihitumihihihi");
+      System.out.println(res);
+   }
+}
