29d819f5200f5e9998cb0144fb70e20b902135f5
Adriana <afuentes321@gmail.com>
2017-03-30 19:34:50 -0700

    Arrays product solution constant time.

--- src/ipractice/ArraysProduct.java
+++ src/ipractice/ArraysProduct.java
@@ -3,7 +3,7 @@ package ipractice;
 /**
  * @author Adriana Fuentes
  * 
- * Given an array of numbers, nums, return
+ * Given an array of numbers, return
  * an array of numbers products, where 
  * products[i] is the product of all 
  * nums[j], j != i
@@ -14,15 +14,46 @@ package ipractice;
  * This must be done in O(N) time and constant space without 
  * using division. Usage of products array is not considered 
  * extra space.
+ * 
  */
 public class ArraysProduct {
    
    /*
+    * Result using products array
+    * Time Complexity: O(n)
+    * Space Complexity: O(1)
+    * 
+    * results [1,1,1,1,1]
+    * 
+    */
+   public static int[] arrayProductConstantTime(int[] data) {
+      if(data.length == 0)
+         return null;
+      
+      int[] results = new int[data.length];
+      for(int i=0; i<data.length; i++) {
+         results[i] = 1;
+      }
+      
+      int prev = data[0];
+      for(int i=1; i<data.length; i++) { 
+         results[i] = prev;
+         prev = prev * data[i];
+      }
+      
+      int fwd = data[data.length-1];
+      for(int i=data.length-2; i>=0; i--) {
+         results[i] = fwd * results[i];
+         fwd = fwd * data[i];
+      }
+      return results;
+   }
+   
+   /*
     * Recursive result
     * Time Complexity: O(n)
-    * Space Complexity: 
-    */
-   
+    * Space Complexity: O(n), stack space due to recursion
+    */   
    public static int product(int[] data, int prev, int index) {
       if(index >= data.length)
          return 1;
@@ -77,5 +108,10 @@ public class ArraysProduct {
       System.out.println("\nDivision Solution");
       arrayProduct(data1);
       printData(data1);
+      
+      int[] data2 = {1, 2, 3, 4, 5};
+      System.out.println("\nConstant Time Solution");
+      int[] results = arrayProductConstantTime(data2);
+      printData(results);
    }
 }
