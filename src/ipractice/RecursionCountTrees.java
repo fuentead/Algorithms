874bc9440854e98b5e36079a6b5c8a795f8ed73b
fuentead <afuentes321@gmail.com>
2017-08-18 12:43:49 -0700

    Recursion Count Trees

--- src/ipractice/RecursionCountTrees.java
+++ src/ipractice/RecursionCountTrees.java
@@ -31,8 +31,7 @@ public class RecursionCountTrees {
       
       int totalSum = 0;
       int leftSum = 0;
-      int rightSum = 0;
-      
+      int rightSum = 0;      
       for(int i=1; i<=nodeCount; i++) {           
          leftSum = recursionCountTrees(i-1);
          rightSum = recursionCountTrees(nodeCount-i);           
