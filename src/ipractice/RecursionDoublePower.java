b3984ce32190bb14071ccef58049787f3e48eeee
fuentead <afuentes321@gmail.com>
2017-08-18 13:11:02 -0700

    Double power

--- src/ipractice/RecursionDoublePower.java
+++ src/ipractice/RecursionDoublePower.java
@@ -14,8 +14,7 @@ package ipractice;
  */
 public class RecursionDoublePower {
    
-   /*
-    * Solution of Double Power with recursion.
+   /* Solution of Double Power with recursion.
     * 
     * Time Complexity: O(n), one option for recursion.
     * Space Complexity: O(1), due to 
