47322734da8117fa7d1361c40e98dfcb65a9eb07
fuentead <afuentes321@gmail.com>
2018-02-09 13:42:22 -0800

    Knights tour on chess board

--- src/ipractice/GraphsKnightsTourOnAChessBoard.java
+++ src/ipractice/GraphsKnightsTourOnAChessBoard.java
@@ -11,64 +11,84 @@ import java.util.*;
  * take to the target location.
  */
 
-class Tuple {
-	int x;
-	int y;
-	int path; 
-	
-	public Tuple(int x, int y, int path) {
-		this.x = x;
-		this.y = y;
-		this.path = path;
-	}
-}
-
 public class GraphsKnightsTourOnAChessBoard {
 	
-	private int size;
 	private int[] kx = {1, 2, 2, 1,-1,-2,-2,-1};
 	private int[] ky = {2, 1,-1,-2,-2,-1, 1, 2};
-	
-	public GraphsKnightsTourOnAChessBoard(int size) {
-		this.size = size;
-	}
-	
-	public boolean isSafe(int x, int y) {
-		if((x < size) && (y <size) && (x >=0) && (y >=0)) 
+	private int size = 8;
+
+	public boolean isSafe(int x, int y, int[][] solution) {
+		if((x < size) && (y <size) && (x >=0) && (y >=0) && (solution[x][y] == -1)) 
 			return true;		
 		else 
 			return false;			
 	}
 	
+
 	/*
-	 * Use BFS for solution
+	 * Use Backtracking for solution
 	 */
-	public int shortestPath(int xstart, int ystart, int xend, int yend){
-		Queue<Tuple> q = new LinkedList<Tuple>();
-		
-		if(!isSafe(xend, yend)) 			
-			return -1;	
-		
-		q.add(new Tuple(xstart, ystart, 0));
-		while(!q.isEmpty()) {
-			Tuple t = q.poll();			
-			if(t.x == xend && t.y == yend) {			
-				return t.path;
-			}
-			
-			for(int i=0; i<size; i++) {
-				int x = t.x + kx[i];
-				int y = t.y + ky[i];
-				if(isSafe(x,y)) 
-					q.add(new Tuple(x,y,t.path+1));		
-			}			
-		}
-		return -1;
+	public boolean knightTour(int xstart, int ystart, int xend, int yend) {
+	   if(xstart == xend && ystart == yend) {
+	      System.out.println("moves: 0");
+	      return true;
+	   }
+	   
+	   int[][] soln = new int[size][size];
+	   for(int i=0; i<size; i++)
+	      for(int j=0; j<size; j++)
+	         soln[i][j] = -1;
+	   soln[xstart][ystart] = 0;
+	   
+	   int nummoves=0;
+	   boolean res = knightTourUtil(xstart, ystart, xend, yend, nummoves+1, soln);
+	   if(res) {
+	      return true;
+	   }
+	   else {
+	      System.out.println("No path");
+	      return false;
+	   }	 
+	}
+	
+	private boolean knightTourUtil(int xstart, int ystart, int xend, int yend, int nummoves, int[][] solution) {
+	   if((xstart == xend) && (ystart == yend)) {
+         System.out.println("moves: " + nummoves);
+         printMoves(solution);
+         return true;
+      }
+	   
+	   int nextmovex;
+	   int nextmovey;
+	   for(int i=0; i<kx.length; i++) {
+	      nextmovey = ystart + ky[i];
+	      nextmovex = xstart + kx[i];
+	      if(isSafe(nextmovex, nextmovey, solution)) {
+	         System.out.println("isSafe " + "nextmovex: " + nextmovex + " nextmovey " + nextmovey);
+	         solution[nextmovex][nextmovey] = nummoves;
+	         boolean res = knightTourUtil(nextmovex, nextmovey, xend, yend, nummoves+1, solution);
+	         if(res) {
+	            return true;
+	         }
+	         else
+	            solution[nextmovex][nextmovey] = -1;
+	      }
+	   }
+	   return false;
+	}
+	
+	private static void printMoves(int[][] solution) {
+	   for(int i=0; i<solution.length; i++){
+	      for(int j=0; j<solution[i].length; j++) {
+	         System.out.print(solution[i][j] + " ");
+	      }
+	      System.out.print("\n");
+	   }
 	}
 	
 	public static void main(String[] args) {
-		GraphsKnightsTourOnAChessBoard cb = new GraphsKnightsTourOnAChessBoard(5);
-		int path = cb.shortestPath(2, 2, 4, 1);
-		System.out.println("path: " + path);
+		GraphsKnightsTourOnAChessBoard cb = new GraphsKnightsTourOnAChessBoard();
+		boolean pathExists = cb.knightTour(2, 1, 6, 2);
+		System.out.println("path: " + pathExists);		
 	}
 }
