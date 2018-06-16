2fe7e55fc630093401c19261922a038e7058cd76
fuentead <afuentes321@gmail.com>
2018-06-16 14:36:20 -0700

    quicksort nuts and bolts

--- src/ipractice/SortingQuicksortNutsAndBolts.java
+++ src/ipractice/SortingQuicksortNutsAndBolts.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package ipractice;
 
 /**
@@ -14,6 +11,21 @@ public class SortingQuicksortNutsAndBolts {
 		QuicksortNutsAndBolts(nuts, bolts, 0, nuts.length-1);
 	}
 	
+   /*
+    * Quicksort method for nuts and bolts
+    * 
+    * Time Complexity: Quicksort complexity n log n
+    * 
+    * 
+    * Space Complexity:
+    * 
+    * 
+    * 1. Partition first bolts first by taking the nuts first element as the pivot.
+    * 2. From the partitioning of bolts, get the middle element and use it as the pivot for nuts
+    * 3. Partition nuts by taking the bolts 'm' element as the pivot.
+    * 4. Use the bolts pivot to do quicksort again from the 'start' to 'boltPivotIndex'-1, to the 
+    * 'boltPivotIndex'+1 to 'end'
+    */
 	public void QuicksortNutsAndBolts(String[] nuts, String[] bolts, int start, int end) {		
 		if(start > end)
 			return;		
@@ -25,15 +37,35 @@ public class SortingQuicksortNutsAndBolts {
 		QuicksortNutsAndBolts(nuts, bolts, boltPivotIndex+1, end);
 	}
 	
+	/*
+	 * Partition is a log n operation.
+	 * 
+	 * 'i' index to iterate toolA
+	 * 'm' index to swap any elements less than the pivot
+	 * 'toolA' is either nuts or bolts passed in. 
+	 * 'start' is the index that will be started at.
+	 * 'end' is the index that will be ended.
+	 * 'tmpValTool' is a variable to hold the start element of toolA. ToolB pivot will go in its place.
+	 * 
+	 * Iterate through array A and:
+	 * 1) Compare if the index 'i' at toolA is less than the pivot
+	 *      If value at 'i' is less than pivot, increase 'm' and swap with 'i', then increase 'i'
+	 * 2) Compare if value at 'i' is equal to the pivot
+	 *      If value is equal, then swap element at 'i' with value from toolA stored at tmpValTool
+	 * 3) Otherwise, increase i
+	 * 
+	 * After completing iteration in toolA, swap value at start index with 'm'
+	 * Also, swap 'm' with 'tmpValTool'
+	 */
 	private int Partition(String[] toolA, int start, int end, String toolBPivot) {
-		int i = start + 1;
-		int m = start;
+		int i = start + 1; // index to iterate in array
+		int m = start;     // index to swap with any element that is less
 		String tmpValTool;		
 		if(start == end)
 			return end;
 		
 		tmpValTool = SwapTempVal(toolA, start, toolBPivot);			
-		while(i <= toolA.length-1) {
+		while(i < toolA.length) {
 			if((int) toolA[i].charAt(1) < (int) toolBPivot.charAt(1)) {// Value is less than pivot
 				m++;
 				Swap(toolA, i, m);	
@@ -70,9 +102,9 @@ public class SortingQuicksortNutsAndBolts {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String[] nuts = {"N7","N4","N2","N5","N1","N6"};
-		String[] bolts = {"B4", "B1", "B6", "B7", "B5", "B2"};		
-		new SortingQuicksortNutsAndBolts().SortNutsAndBolts(nuts, bolts);
+		String[] nuts = {"N3","N4","N2","N5","N1","N6"};
+		String[] bolts = {"B4", "B1", "B6", "B3", "B5", "B2"};		
+		new SortingQuicksortNutsAndBolts().SortNutsAndBolts(nuts, bolts);		
 	}
 
 }
