73eb09746860b3c9810ab6bd06b4345867f97fc7
fuentead <afuentes321@gmail.com>
2018-10-09 14:34:38 -0700

    BSt to doubly circular linked list

---
+++ ipractice/TreesBSTtoCircularDoublyLinkedList.java
@@ -0,0 +1,24 @@
+package ipractice;
+
+/**
+ * @author Adriana Fuentes
+ * 
+ * Write a recursive function treeToList(Node root) that takes a 
+ * BST and rearranges the internal pointers to make a circular 
+ * doubly linked list out of the tree nodes. The "previous" pointers
+ * should be stored in the "Left" field and the "next" pointers should
+ * be stored in the "Right" field. The list should be arranged so that
+ * the nodes are in increasing order. Return the head pointer to the 
+ * new list. The operation can be done in O(n) time.
+ */
+public class TreesBSTtoCircularDoublyLinkedList {
+
+   public void treeToList(Node head) {
+      
+   }
+   
+   public static void main(String[] args) {
+      
+
+   }
+}
